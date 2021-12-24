e2173f7180eb8f18cf4a72ced97e227483d2c535
Evgeny Bogomoloy <evgeny.bogomoloy@technia.com>
2021-12-24 14:00:27 +0100

    Adding class to represent token

---
+++ CompoundSimulator/CompoundSimulator.Library/Token.cs
@@ -0,0 +1,19 @@
+﻿namespace CompoundSimulator.Library
+{
+    public class Token
+    {
+        public string Name { get; set; }
+        public string FullName { get; set; }
+        public decimal CurrentPrice { get; set; }
+        public int DecimalPlaces { get; set; } = 5;
+        public decimal GetPriceIn(Token token)
+        {
+            return this.CurrentPrice / token.CurrentPrice;
+        }
+
+        public string GetPriceStringIn(Token token)
+        {
+            return string.Format("{0:F" + token.DecimalPlaces + "}", this.CurrentPrice / token.CurrentPrice);
+        }
+    }
+}
---
+++ CompoundSimulator/CompoundSimulator.TestClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using CompoundSimulator.Library;
+
+namespace CompoundSimulator.TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var euro = new Token()
+            {
+                Name = "EUR",
+                CurrentPrice = 1,
+            };
+
+            var cro = new Token()
+            {
+                Name = "CRO",
+                DecimalPlaces = 3,
+                CurrentPrice = 0.5222m,
+            };
+
+            Console.WriteLine($"CRO price is {cro.GetPriceIn(euro)} {euro.Name}");
+            Console.WriteLine($"EUR price is {euro.GetPriceIn(cro)} {cro.Name}");
+            Console.WriteLine($"EUR price is {euro.GetPriceStringIn(cro)} {cro.Name}");
+        }
+    }
+}
